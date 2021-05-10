90a8e33f474b3f7e8205b5504334f7b338ff3e96
Ahmed Medhat <ahmed.medhat.2029@gmail.com>
2021-05-10 15:41:06 +0200

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky-nav');
+        } else {
+            $('nav').removeClass('sticky-nav');
+        }
+    }, {
+        offset: '60px;'
+    });
+
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    /* Mobile nav */
+    $('.mobile-nav-icon').click(function () {
+        var nav = $('.main-nav');
+        nav.slideToggle(200);
+        
+        var icon = $('.mobile-nav-icon i');
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+});
